76df73fe51d5a36ab8259207dd99f5954d7fc24e
Waldemar <vladdigen@gmail.com>
2024-04-27 17:56:54 +0400

    Remove the unnecessary files

--- bin/weather_console_app.dart
+++
@@ -1,5 +0,0 @@
-import 'package:weather_console_app/weather_console_app.dart' as weather_console_app;
-
-void main(List<String> arguments) {
-  print('Hello world: ${weather_console_app.calculate()}!');
-}
