043312cb9ad79f20c038ac1d8180dd77b758d3d8
Waldemar <vladdigen@gmail.com>
2024-04-27 17:57:10 +0400

    Create weather model

---
+++ lib/weather.dart
@@ -0,0 +1,26 @@
+class Weather {
+  final String lastUpdated;
+  final String cityName;
+  final double tempC;
+  final double feelsLikeC;
+  final String condition;
+
+  Weather({
+    required this.lastUpdated,
+    required this.cityName,
+    required this.tempC,
+    required this.feelsLikeC,
+    required this.condition,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    final currentData = json['current'];
+    return Weather(
+      lastUpdated: currentData['last_updated'],
+      cityName: json['location']['name'],
+      tempC: currentData['temp_c'].toDouble(),
+      feelsLikeC: currentData['feelslike_c'].toDouble(),
+      condition: currentData['condition']['text'],
+    );
+  }
+}
